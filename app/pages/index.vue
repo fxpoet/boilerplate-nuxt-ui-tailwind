0c15e3950cec85a0442fc53a54909edb07f618da
sabzil <im@fxpoet.com>
2025-11-06 18:29:13 +0800

    added pinia

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,12 @@
     <h1 class="text-3xl font-bold underline">
       안녕하세요!
     </h1>
-    <UButton>Click Me</UButton>
+    <UButton @click="store.increment()">Click Me</UButton>
     <UIcon name="i-lucide-lightbulb" class="size-5" />
+    <div>{{ store.count }}</div>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter';
+const store = useCounterStore();
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,28 @@
 import tailwindcss from "@tailwindcss/vite";
 
-
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxt/scripts',
-    '@nuxt/image'
-  ],
+    modules: [
+        '@nuxt/test-utils',
+        '@nuxt/ui',
+        '@nuxt/eslint',
+        '@nuxt/scripts',
+        '@nuxt/image',
+        '@pinia/nuxt'
+    ],
 
-  css: ['./app/assets/main.css'],
+    css: ['./app/assets/main.css'],
 
-  vite: {
-    plugins: [
-      tailwindcss()
-    ]
-  }
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    },
+
+    pinia: {
+        storesDirs: ['./stores/**', './app/stores/**'],
+    }
+
 })
